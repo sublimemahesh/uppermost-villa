919d2c39cd198469e9fec6adaf5f20837d6bcba6
ADinushika <amalidinushika93@gmail.com>
2018-07-09 14:09:40 +0530

    #36 Create view-excursion.php page

---
+++ view-excursion.php
@@ -0,0 +1,204 @@
+<!DOCTYPE html>
+<html lang="en-US" class="scheme_original">
+
+    <head>
+
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+        <meta name="format-detection" content="telephone=no">
+        <link rel="icon" type="image/x-icon" href="images/favicon.png" />
+        <title>Uppermost Villa</title>
+        <link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Droid+Serif%3A400%2C700%7CRaleway%3A100%2C200%2C300%2C400%2C500%2C600%2C700%2C800%2C900%7COpen+Sans%3A300%2C400%2C600%2C700%2C800%7CMontserrat%3A700%2C400&amp;subset=cyrillic%2Ccyrillic-ext%2Clatin%2Cgreek-ext%2Cgreek%2Clatin-ext%2Cvietnamese&amp;ver=1.6.11" type="text/css" media="all">
+        <link rel='stylesheet' href='js/vendor/booked/font-awesome.min.css' type='text/css' media='all' />
+        <link rel='stylesheet' href='js/vendor/essgrid/tooltipster.css' type='text/css' media='all' />
+        <link rel='stylesheet' href='js/vendor/essgrid/tooltipster-light.css' type='text/css' media='all' />
+        <link rel='stylesheet' href='js/vendor/booked/styles.css' type='text/css' media='all' />
+        <link rel='stylesheet' href='js/vendor/revslider/settings.css' type='text/css' media='all' />
+        <link rel='stylesheet' href='css/fontello/css/fontello.css' type='text/css' media='all' />
+        <link rel='stylesheet' href='css/custom/_animation.css' type='text/css' media='all' />
+        <link rel='stylesheet' href='css/custom/shortcodes.css' type='text/css' media='all' />
+        <link rel='stylesheet' href='css/style.css' type='text/css' media='all' />
+        <link rel='stylesheet' href='css/custom/skin.css' type='text/css' media='all' />
+        <link rel='stylesheet' href='css/custom/custom-style.css' type='text/css' media='all' />
+        <link rel='stylesheet' href='css/custom/colors.css' type='text/css' media='all' />
+        <link rel='stylesheet' href='css/custom/responsive.css' type='text/css' media='all' />
+        <link rel='stylesheet' href='css/custom/skin.responsive.css' type='text/css' media='all' />
+        <link rel='stylesheet' href='js/vendor/swiper/swiper.css' type='text/css' media='all' />
+        <link rel='stylesheet' href='css/custom/_messages.css' type='text/css' media='all' />
+        <link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
+        <link href="js/vendor/magnific-popup/magnific-popup.css" rel="stylesheet" type="text/css"/>
+        <link href="js/vendor/booked/booked.css" rel="stylesheet" type="text/css"/>
+        <link href="css/custom/instagram-widget.css" rel="stylesheet" type="text/css"/>
+        <link href="js/vendor/magnific-popup/magnific-popup.css" rel="stylesheet" type="text/css"/>
+
+
+    </head>
+    <body class="body_style_wide responsive_menu scheme_original top_panel_show top_panel_above sidebar_hide">
+        <div class="body_wrap">
+            <div class="page_wrap">
+
+                <!-- Header -->
+                <?php
+                include 'header.php';
+                ?>
+                <!-- /Header -->
+
+                <!--breadcrumbs-->
+                <div class="top_panel_title heading-banner top_panel_style_1  title_present scheme_original">
+                    <div class="top_panel_title_inner top_panel_inner_style_1 breadcrumbs_present_inner">
+                        <div class="content_wrap">
+                            <h1 class="page_title banner-title">View Excursion</h1>
+                            <div class="breadcrumbs banner-title">
+                                <a class="breadcrumbs_item home" href="./">Home</a>
+                                <span class="breadcrumbs_delimiter"></span>
+                                <a class="breadcrumbs_item home" href="excursion.php">Excursions</a>
+                                <span class="breadcrumbs_delimiter"></span>
+                                <span class="breadcrumbs_item current">View Excursion</span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!--/breadcrumbs-->
+
+                <div class="page_content_wrap accommo-padding">
+                    <div class="content_wrap">
+                        <div class="content">
+                            <section class="post_featured">
+                                <div class="post_thumb image-view">
+                                    <a class="hover_icon hover_icon_view" href="images/img-600x410.jpg" title="87 Mishaum Point Rd, Dartmouth, MA 02748">
+                                        <img alt="" src="images/img-600x410.jpg"></a>
+                                </div>
+                            </section>
+                            <section class="post_content">
+                                <h3 class="post_title">Deluxe Double room with ocean view</h3>
+                                
+                                <div class="sc_section">
+                                    <p class="text-justify">The Moana Residence is situated on the best lot at Kohanaiki and is a 5 bedroom, 5 1/2 bath home of approximately 8,000 interior sq. ft. It features Ipe hardwood flooring on the interior and granite stone flooring on the lanais, granite countertops, vaulted cedar ceilings, clerestory windows for lots of light.</p>
+                                    <p class="text-justify">mahogany cabinetry, mahogany trim and pocketing doors throughout. The beautifully landscaped grounds include a large lap pool, spa and separate Pool.</p>
+                                    <p class="text-justify">Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.</p>
+                                    <p class="text-justify text-bottom">There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc.</p>                 
+                                </div>
+                            </section>
+                        </div>
+                    </div>
+                </div>
+                <div class="custom_shortcode_box">
+                    <div class="content_wrap">
+                        <h4 class="sc_title">More Excursions</h4>
+                        <div class="sc_property_wrap">
+                            <div class="sc_property sc_property_style_property-1 " data-interval="5446" data-slides-per-view="3">
+                                <div class="sc_columns columns_wrap">
+                                    <div class="column-1_3 column_padding_bottom">
+                                        <div class="sc_property_item">
+                                            <div class="sc_property_image">
+                                                <a href="">                                                  
+                                                    <img alt="" src="images/image-7-1-770x460.jpg">
+                                                </a>
+                                            </div>
+                                            <div class="sc_property_info">
+                                                <div>
+                                                    <div class="sc_property_title">
+                                                        <div class="sc_property_title_address_1">
+                                                            <a href="#">87 Mishaum Point Rd</a> 
+                                                        </div>
+                                                        <div class="text-justify accommo-property-content">Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa...</div>
+                                                    </div>
+                                                    <div class="cL"></div>
+                                                </div>
+                                            </div>
+                                            <div class="sc_property_info_list">                                          
+                                                <button class="read-more excursion-readmore">Read More</button>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="column-1_3 column_padding_bottom">
+                                        <div class="sc_property_item">
+                                            <div class="sc_property_image">
+                                                <a href="">                                                  
+                                                    <img alt="" src="images/image-7-1-770x460.jpg">
+                                                </a>
+                                            </div>
+                                            <div class="sc_property_info">
+                                                <div>
+                                                    <div class="sc_property_title">
+                                                        <div class="sc_property_title_address_1">
+                                                            <a href="#">87 Mishaum Point Rd</a> 
+                                                        </div>
+                                                        <div class="text-justify accommo-property-content">Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa...</div>
+                                                    </div>
+                                                    <div class="cL"></div>
+                                                </div>
+                                            </div>
+                                            <div class="sc_property_info_list">                                          
+                                                <button class="read-more excursion-readmore">Read More</button>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="column-1_3 column_padding_bottom">
+                                        <div class="sc_property_item">
+                                            <div class="sc_property_image">
+                                                <a href="">                                                  
+                                                    <img alt="" src="images/image-7-1-770x460.jpg">
+                                                </a>
+                                            </div>
+                                            <div class="sc_property_info">
+                                                <div>
+                                                    <div class="sc_property_title">
+                                                        <div class="sc_property_title_address_1">
+                                                            <a href="#">87 Mishaum Point Rd</a> 
+                                                        </div>
+                                                        <div class="text-justify accommo-property-content">Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa...</div>
+                                                    </div>
+                                                    <div class="cL"></div>
+                                                </div>
+                                            </div>
+                                            <div class="sc_property_info_list">                                          
+                                                <button class="read-more excursion-readmore">Read More</button>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <!-- Footer -->
+                <?php
+                include 'footer.php';
+                ?>
+                <!-- /Footer -->
+
+            </div>
+        </div>
+        <a href="#" class="scroll_to_top icon-up"></a>
+
+        <script data-cfasync="false" src="cdn-cgi/scripts/f2bf09f8/cloudflare-static/email-decode.min.js"></script><script type='text/javascript' src='js/vendor/jquery.js'></script>
+        <script type='text/javascript' src='js/custom/plugins.js'></script>
+        <script type='text/javascript' src='js/custom/messages.js'></script>
+        <script type='text/javascript' src='js/vendor/jquery-migrate.min.js'></script>
+        <script type='text/javascript' src='js/vendor/essgrid/lightbox.js'></script>
+        <script type='text/javascript' src='js/vendor/essgrid/jquery.themepunch.tools.min.js'></script>
+        <script type='text/javascript' src='js/vendor/revslider/jquery.themepunch.revolution.min.js'></script>
+        <script type='text/javascript' src='js/vendor/modernizr.min.js'></script>
+        <script type='text/javascript' src='js/vendor/ui/jquery-ui.min.js'></script>
+        <script type="text/javascript" src="js/vendor/revslider/revolution.extension.slideanims.min.js"></script>
+        <script type="text/javascript" src="js/vendor/revslider/revolution.extension.layeranimation.min.js"></script>
+        <script type="text/javascript" src="js/vendor/revslider/revolution.extension.navigation.min.js"></script>
+        <script type='text/javascript' src='js/vendor/superfish.js'></script>
+        <script type='text/javascript' src='js/custom/_utils.js'></script>
+        <script type='text/javascript' src='js/custom/_init.js'></script>
+        <script type='text/javascript' src='js/custom/_shortcodes.js'></script>
+        <script type='text/javascript' src='js/vendor/parallax.js'></script>
+        <script type='text/javascript' src='js/vendor/skrollr.min.js'></script>
+        <script type='text/javascript' src='js/vendor/swiper/swiper.min.js'></script>
+        <script src="js/vendor/isotope.min.js" type="text/javascript"></script>
+        <script type='text/javascript' src='http://maps.google.com/maps/api/js?sensor=false'></script>
+        <script src="js/custom/_googlemap.js" type="text/javascript"></script>
+        <script src="js/vendor/magnific-popup/jquery.magnific-popup.min.js" type="text/javascript"></script>
+
+        <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+
+    </body>
+
+</html>
